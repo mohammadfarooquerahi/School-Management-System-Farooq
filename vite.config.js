113ab1e30c74331ff250575421c39b3e4ef0ca24
farooqrahi006 <farooqrahi828@gmail.com>
2026-05-01 12:44:25 +0500

    Completed fix all  vite base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/my-School-Management-System-smitaa/", // ✅ add this
+  base: "/School-Management-System-Farooq/", // ✅ add this
   plugins: [react(), tailwindcss()],
 });
